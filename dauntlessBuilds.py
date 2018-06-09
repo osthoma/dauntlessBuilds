20ffbb2e9af8807d6ab878d1c5d2648bc26a7eb9
Oskar Thoma <thomaoskar@googlemail.com>
2018-06-09 16:37:51 +0200

    major layout update

--- dauntlessBuilds.py
+++ dauntlessBuilds.py
@@ -11,10 +11,13 @@ with open("json/weaponList.json") as f:
 with open("json/cellList.json") as f:
     cellData = json.load(f)
 
+with open("json/lanternList.json") as f:
+    lanternData = json.load(f)
+
 
 @app.route('/')
 def index():
-    return render_template('home.html', weaponData=weaponData, armorData=armorData)
+    return render_template('home.html', weaponData=weaponData, armorData=armorData, cellData=cellData, lanternData=lanternData)
 
 
 if __name__ == '__main__':
