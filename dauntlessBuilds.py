d5cf749ecea88e44f586ad5df5781044b67c66f0
Oskar Thoma <thomaoskar@googlemail.com>
2018-06-09 14:48:21 +0200

    adds dropdown selection

--- dauntlessBuilds.py
+++ dauntlessBuilds.py
@@ -1,10 +1,20 @@
+import json
 from flask import Flask, render_template
 app = Flask(__name__)
+
+with open("json/armorList.json") as f:
+    armorData = json.load(f)
+
+with open("json/weaponList.json") as f:
+    weaponData = json.load(f)
+
+with open("json/cellList.json") as f:
+    cellData = json.load(f)
 
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', weaponData=weaponData, armorData=armorData)
 
 
 if __name__ == '__main__':
